50831691435861b4e5326dfd9392b8f5f4c2902c
userrtm <masterbigserverclas@gmail.com>
2026-05-03 13:17:57 +0500

    Update [key].js

--- api/standart/[key].js
+++ api/standart/[key].js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   if (!key) return res.status(400).send("missing key");
 
-  const target = https://standart.lexum.sbs/sub/${encodeURIComponent(key)};
+  const target = `https://standartuserrtm.pro/proxy/vpn/sub/${encodeURIComponent(key)}`;
 
   try {
     const r = await fetch(target, {
